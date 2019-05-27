b0df6b1804b3149f4d6e2fccaba099a160a2a880
Eugene <Legsdogs@gmail.com>
2019-05-27 11:41:40 +0300

    test

--- app/src/main/java/com/example/final_progect/Item.java
+++ app/src/main/java/com/example/final_progect/Item.java
@@ -2,6 +2,10 @@ package com.example.final_progect;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class Item extends View{
@@ -11,7 +15,14 @@ public class Item extends View{
     int value_time;
 
 
-    public Item(Context context) {
-        super(context);
+    public Item(Context context,  @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint p =
+        super.onDraw(canvas);
     }
 }
+
--- app/src/main/java/com/example/final_progect/NetView.java
+++ app/src/main/java/com/example/final_progect/NetView.java
@@ -10,23 +10,79 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.TreeMap;
+
 public class NetView extends View {
-    private int widthView;
-    private int heightView;
+    private float width;
+    private float height;
+
+
+    private TreeMap<Float, Float> list = new TreeMap<>();
 
 
     public NetView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+    Paint p = new Paint();
+
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        float lenght = canvas.getWidth()/50;
-        float hight = canvas.getHeight()/100;
-        for (float x = 0; x < lenght*50; x+= lenght) {
-            for (float y = 0; y < hight; y = y + a) {
-                canvas.drawRect(x, y, x + a, y + a, p);
+
+        p.setColor(Color.BLACK);
+        p.setStyle(Paint.Style.STROKE);
+
+        if (canvas != null) {
+            canvas.drawColor(Color.WHITE);
+
+            width = canvas.getWidth();
+            height = canvas.getHeight();
+
+            float h = width / 50;
+            float w = height / 100;
+            for (float x = 0; x < 20 * h; x = x + h) {
+                for (float y = 0; y < 28 * w; y = y + w) {
+                    canvas.drawRect(x, y, x + h, y + w, p);
+                }
+                onAddParams(h, w, canvas);
+            }
+            super.onDraw(canvas);
+        }
+
+
+    }
+
+    private float touchX;
+    private float touchY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        touchX = event.getX();
+        touchY = event.getY();
+        return super.onTouchEvent(event);
+    }
+
+
+    private void onAddParams(float xt, float yt, Canvas cavas) {
+        for (float x = 0; x < 50 * xt; x = x + xt) {
+            for (float y = 0; y < 100 * yt; y = y + yt) {
+                if ((touchX >= x && touchX <= x + xt)) {
+                    if (touchY >= y && touchY <= y + yt) {
+                        p.setColor(Color.BLUE);
+                        p.setStyle(Paint.Style.FILL);
+                        cavas.drawRect(x - xt, y- yt, x+xt,y+yt, p);
+
+                        //draw
+                        p.setColor(Color.BLACK);
+                        p.setStyle(Paint.Style.STROKE);
+                        if (list.containsKey(y)){
+                            list.put(x, y);
+                            //draw privious rectiangle
+                        }else{
+                            list.put(x, y);
+                        }
+                    }
+                }
             }
         }
     }
